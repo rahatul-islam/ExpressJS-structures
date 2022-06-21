ce49550a658baffaf8fc3dbbc037610feb27a41f
Rahat <mdrahatulislam121@.com>
2022-06-21 18:32:35 +0600

    create login controler

--- controllers/authController.js
+++ controllers/authController.js
@@ -39,11 +39,33 @@ exports.signupPostController = async (req, res, next) => {
 
 
 exports.loginGetController = (req, res, next) => {
-    res.render('pages/auth/login', {title:'Log in your account'})
+    res.render('pages/auth/login', {title:'Log in to your account'})
 }
 
-exports.loginPostController = (req, res, next) => {
+exports.loginPostController = async (req, res, next) => {
+    let{email,password}=req.body
 
+    try {
+        let user = await User.findOne({email})
+        if (!user) {
+            return res.json({
+                messege: 'Invalid Credential'
+            })
+        }
+
+        let match= await bcrypt.compare(password, user.password)
+        if (!match) {
+            return res.json({
+                messege:'Invalid Credential'
+            })
+        }
+
+        console.log('Succesfully Logged in', user)
+        res.render('/pages/auth/login', {title:'Log in to your account'})
+    } catch (e) {
+        console.log(e)
+        next(e)
+    }
 }
 
 exports.logoutController = (req, res, next) => {
