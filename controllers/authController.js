e5cc10e1a94578fbc35cd4417c5bf165d9b726ba
Rahat <mdrahatulislam121@.com>
2022-06-27 13:15:07 +0600

    render submitted data back

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,30 +1,36 @@
 const bcrypt = require('bcrypt')
-const {validationResult}=require('express-validator')
+const {
+    validationResult
+} = require('express-validator')
 
 const User = require('../models/User')
 
-const errorFormatter=require('../utils/validationErrorFormatter')
+const errorFormatter = require('../utils/validationErrorFormatter')
 
 exports.signupGetController = (req, res, next) => {
     res.render('pages/auth/signup', {
-        title: 'Create a New Account',error:{}
+        title: 'Create a New Account',
+        error: {},
+        value: {}
     })
 }
 
 exports.signupPostController = async (req, res, next) => {
-
-    let errors=validationResult(req).formatWith(errorFormatter)
-    if (!errors.isEmpty()) {
-        return res.render('pages/auth/signup', {
-            title: 'Create a New Account', error: errors.mapped()
-        })
-    }
-
     let {
         username,
         email,
         password
     } = req.body
+
+    let errors = validationResult(req).formatWith(errorFormatter)
+    if (!errors.isEmpty()) {
+        return res.render('pages/auth/signup', {
+            title: 'Create a New Account',
+            error: errors.mapped(),
+            value: {username,email,password}
+        })
+    }
+
 
     try {
         let hashedPassword = await bcrypt.hash(password, 11)
@@ -48,29 +54,38 @@ exports.signupPostController = async (req, res, next) => {
 }
 
 exports.loginGetController = (req, res, next) => {
-    res.render('pages/auth/login', {title:'Log in to your account'})
+    res.render('pages/auth/login', {
+        title: 'Log in to your account'
+    })
 }
 
 exports.loginPostController = async (req, res, next) => {
-    let{email,password}=req.body
+    let {
+        email,
+        password
+    } = req.body
 
     try {
-        let user = await User.findOne({email})
+        let user = await User.findOne({
+            email
+        })
         if (!user) {
             return res.json({
                 messege: 'Invalid Credential'
             })
         }
 
-        let match= await bcrypt.compare(password, user.password)
+        let match = await bcrypt.compare(password, user.password)
         if (!match) {
             return res.json({
-                messege:'Invalid Credential'
+                messege: 'Invalid Credential'
             })
         }
 
         console.log('Succesfully Logged in', user)
-        res.render('/pages/auth/login', {title:'Log in to your account'})
+        res.render('/pages/auth/login', {
+            title: 'Log in to your account'
+        })
     } catch (e) {
         console.log(e)
         next(e)
